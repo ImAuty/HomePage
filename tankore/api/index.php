ddcb4c1104200f4f8204addf6f2c82411f934d86
ImAuty <DeveloperAuty@gmail.com>
2022-11-22 02:05:00 +0900

    DBを利用してデータの取得を行うように修正 変数名や取得方法、デザインを変更

--- tankore/api/index.php
+++ tankore/api/index.php
@@ -1,62 +1,249 @@
 <?php
-// JSONを出力する
-header('Content-Type: application/json; charset=UTF-8');
+// DB接続クラス
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../php/DbConnection.php');
 
-$inputs = [
-    "numberOfLetters" => 7,
-    "numberOfAnswers" => 6,
-    "letters" => [
-        ["key" => "a",  "value" => "あ", "isDisabled" => false],
-        ["key" => "i",  "value" => "い", "isDisabled" => false],
-        ["key" => "u",  "value" => "う", "isDisabled" => false],
-        ["key" => "e",  "value" => "え", "isDisabled" => false],
-        ["key" => "o",  "value" => "お", "isDisabled" => false],
-        ["key" => "ka", "value" => "か", "isDisabled" => false],
-        ["key" => "ki", "value" => "き", "isDisabled" => false],
-        ["key" => "ku", "value" => "く", "isDisabled" => false],
-        ["key" => "ke", "value" => "け", "isDisabled" => false],
-        ["key" => "ko", "value" => "こ", "isDisabled" => false],
-        ["key" => "sa", "value" => "さ", "isDisabled" => false],
-        ["key" => "si", "value" => "し", "isDisabled" => false],
-        ["key" => "su", "value" => "す", "isDisabled" => false],
-        ["key" => "se", "value" => "せ", "isDisabled" => false],
-        ["key" => "so", "value" => "そ", "isDisabled" => false],
-        ["key" => "ta", "value" => "た", "isDisabled" => false],
-        ["key" => "ti", "value" => "ち", "isDisabled" => false],
-        ["key" => "tu", "value" => "つ", "isDisabled" => false],
-        ["key" => "te", "value" => "て", "isDisabled" => false],
-        ["key" => "to", "value" => "と", "isDisabled" => false],
-        ["key" => "na", "value" => "な", "isDisabled" => false],
-        ["key" => "ni", "value" => "に", "isDisabled" => false],
-        ["key" => "nu", "value" => "ぬ", "isDisabled" => false],
-        ["key" => "ne", "value" => "ね", "isDisabled" => false],
-        ["key" => "no", "value" => "の", "isDisabled" => false],
-        ["key" => "ha", "value" => "は", "isDisabled" => false],
-        ["key" => "hi", "value" => "ひ", "isDisabled" => false],
-        ["key" => "hu", "value" => "ふ", "isDisabled" => false],
-        ["key" => "he", "value" => "へ", "isDisabled" => false],
-        ["key" => "ho", "value" => "ほ", "isDisabled" => false],
-        ["key" => "ma", "value" => "ま", "isDisabled" => false],
-        ["key" => "mi", "value" => "み", "isDisabled" => false],
-        ["key" => "mu", "value" => "む", "isDisabled" => false],
-        ["key" => "me", "value" => "め", "isDisabled" => false],
-        ["key" => "mo", "value" => "も", "isDisabled" => false],
-        ["key" => "ya", "value" => "や", "isDisabled" => false],
-        ["key" => "yu", "value" => "ゆ", "isDisabled" => false],
-        ["key" => "yo", "value" => "よ", "isDisabled" => false],
-        ["key" => "_1", "value" => "　", "isDisabled" => true],
-        ["key" => "_2", "value" => "　", "isDisabled" => true],
-        ["key" => "ra", "value" => "ら", "isDisabled" => false],
-        ["key" => "ri", "value" => "り", "isDisabled" => false],
-        ["key" => "ru", "value" => "る", "isDisabled" => false],
-        ["key" => "re", "value" => "れ", "isDisabled" => false],
-        ["key" => "ro", "value" => "ろ", "isDisabled" => false],
-        ["key" => "wa", "value" => "わ", "isDisabled" => false],
-        ["key" => "wo", "value" => "を", "isDisabled" => false],
-        ["key" => "nn", "value" => "ん", "isDisabled" => false],
-        ["key" => "ds", "value" => "ー", "isDisabled" => false],
-        ["key" => "mp", "value" => "・", "isDisabled" => false],
-    ]
-];
+// 定数
+/** 日付フォーマット */
+const DATE_FORMAT = 'Y-m-d';
+/** 対応メソッドリスト */
+const ALLOWED_METHODS = ['GET', 'POST'];
+/** ブロック */
+const CHARACTER_BLOCK = 'Katakana'; // カタカナ
+/** パターン */
+const LETTER_PATTERN = '/[ァ-ワヲンー]/u'; // カタカナの中で記号や一般的でない文字を含まない表現
 
-echo json_encode($inputs);
+try {
+    // JSONヘッダー
+    header('Content-Type: application/json; charset=UTF-8');
+    // 対応メソッドを確認する
+    if (!in_array($_SERVER['REQUEST_METHOD'], ALLOWED_METHODS, true)) {
+        // HTTPレスポンスコード:Method Not Allowed(405)
+        http_response_code(405);
+        // JSONボディ
+        echo json_encode([
+            'errorMessage' => '対応しないメソッドです。'
+        ]);
+        exit;
+    }
+
+    // 対象日付を設定する
+    $targetDate = new DateTime();
+    // 回答単語を設定する
+    $replyWord = null;
+    // リクエストメソッドを確認する
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    if ($requestMethod === 'POST') {
+        // リクエストボディを取得する
+        $requestBody = file_get_contents('php://input');
+        $requestData = json_decode($requestBody, true);
+        // 対象日付を取得する
+        $dateStr = $requestData['answerDate'];
+        $targetDate = DateTime::createFromFormat(DATE_FORMAT, $dateStr);
+        // 回答単語を取得する
+        $replyWord = $requestData['inputWord'];
+    }
+
+    // DBに接続する
+    $dbh = DbConnection::connectTankore();
+
+    // 回答失敗かを設定する
+    $isFailedToReply = false;
+    // 回答単語を確認する
+    if (isset($replyWord)) {
+        // 回答単語が存在するか確認する
+        $wordCount = countWordByValue($dbh, $replyWord);
+        // 回答単語が存在しない場合は回答失敗を設定する
+        $isFailedToReply = ($wordCount !== 1);
+    }
+
+    // 解答を取得する
+    $answer = findAnswerByDate($dbh, $targetDate);
+    // 解答単語を取得する
+    $answerWord = $answer['word'];
+    // 解答文字数を取得する
+    $numberOfAnswerLetters = mb_strlen($answerWord);
+    // 解答制限を取得する
+    $answerLimit = $answer['limit'];
+    // 解答日付を取得する
+    $answerDate = $answer['date'];
+
+    // 回答単語リストを設定する
+    $replyWords = [];
+    // セッションを開始する
+    session_start();
+    // 対象日付からセッションキーを作成する
+    $sessionKey = $targetDate->format(DATE_FORMAT);
+    // セッションを確認する
+    if (isset($_SESSION[$sessionKey])) {
+        // セッションを取得する
+        $replyWords = $_SESSION[$sessionKey];
+    } else {
+        // セッションを解除する
+        $_SESSION = [];
+    }
+    // 回答単語を確認する
+    if (isset($replyWord) && !$isFailedToReply) {
+        // 回答単語をリストに追加する
+        if (count($replyWords) < $answerLimit) $replyWords[] = $replyWord;
+    }
+    // セッションを設定する
+    $_SESSION[$sessionKey] = $replyWords;
+    // セッションを終了する
+    session_write_close();
+
+    // 回答リストを作成する
+    $replies = createReplies($replyWords, $answerWord);
+
+    // 文字リスト（記号などを含む）を取得する
+    $characterBlock = CHARACTER_BLOCK;
+    $characters = findCharactersByBlock($dbh, $characterBlock);
+    // 文字リスト（記号などを含まない）を作成する
+    $pattern = LETTER_PATTERN;
+    $letters = createLetters($characters, $pattern);
+
+    // HTTPレスポンスコード:OK(200)
+    http_response_code(200);
+    // JSONボディ
+    echo json_encode([
+        'isFailedToReply' => $isFailedToReply,
+        'numberOfAnswerLetters' => $numberOfAnswerLetters,
+        'answerLimit' => $answerLimit,
+        'targetDate' => $answerDate,
+        'replies' => $replies,
+        'letters' => $letters,
+    ]);
+} catch (RuntimeException $e) {
+    // HTTPレスポンスコード:Internal Server Error(500)
+    http_response_code(500);
+    // JSONボディ
+    echo json_encode([
+        'errorMessage' => $e->getMessage()
+    ]);
+} catch (Throwable $e) {
+    // HTTPレスポンスコード:Internal Server Error(500)
+    http_response_code(500);
+    // JSONボディ
+    echo json_encode([
+        'errorMessage' => '想定外の事象が発生しました。'
+    ]);
+}
+
+/**
+ * 単語の数を取得する
+ * 
+ * @param PDO    $dbh   PDO
+ * @param string $value 値
+ * @return int 単語の数
+ */
+function countWordByValue(PDO $dbh, string $value): int
+{
+    $sql = 'SELECT COUNT(*) FROM `words` WHERE `value` = :value';
+    $sth = $dbh->prepare($sql);
+    $sth->bindValue('value', $value, PDO::PARAM_STR);
+    $sth->execute();
+    $result = $sth->fetchColumn();
+    return $result;
+}
+
+/**
+ * 文字リストを取得する
+ * 
+ * @param PDO    $dbh   PDO
+ * @param string $block ブロック
+ * @return array 文字リスト
+ */
+function findCharactersByBlock(PDO $dbh, string $block): array
+{
+    $sql = 'SELECT * FROM `characters` WHERE `block` = :block';
+    $sth = $dbh->prepare($sql);
+    $sth->bindValue('block', $block, PDO::PARAM_STR);
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+/**
+ * 解答を取得する
+ * 
+ * @param PDO      $dbh  PDO
+ * @param DateTime $date 日付
+ * @return mixed 解答
+ */
+function findAnswerByDate(PDO $dbh, DateTime $date): mixed
+{
+    $sql = 'SELECT * FROM `answers` WHERE `date` = :date';
+    $sth = $dbh->prepare($sql);
+    $dateStr = $date->format('Y-m-d');
+    $sth->bindValue('date', $dateStr, PDO::PARAM_STR);
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    if (count($result) !== 1) throw new RuntimeException("解答の取得に失敗しました。");
+    return $result[0];
+}
+
+/**
+ * 回答リストを作成する
+ * 
+ * @param array  $replyWords 回答単語リスト
+ * @param string $answerWord 解答単語
+ * @return array 回答リスト
+ */
+function createReplies(array $replyWords, string $answerWord): array
+{
+    // 解答文字リストを取得する
+    $answerLetters = mb_str_split($answerWord);
+    // 回答リストを設定する
+    $replies = [];
+    foreach ($replyWords as $replyWord) {
+        // 回答文字リストを取得する
+        $replyLetters = mb_str_split($replyWord);
+        // 回答を設定する
+        $reply = [];
+        foreach ($replyLetters as $i => $replyLetter) {
+            // 文字を設定する
+            $letter = [];
+            // 回答文字を設定する
+            $letter['value'] = $replyLetter;
+            // 回答文字が解答単語に含まれる数を設定する
+            $contains = array_filter($answerLetters, fn ($l) => $l === $replyLetter);
+            $letter['inAnswerWord'] = count($contains);
+            // 回答文字が解答文字と一致するかを設定する
+            $letter['matchesAnswerLetter'] = false;
+            if (count($answerLetters) > $i) {
+                $answerLetter = $answerLetters[$i];
+                $letter['matchesAnswerLetter'] = ($replyLetter === $answerLetter);
+            }
+            // 文字をリストに追加する
+            $reply[$i] = $letter;
+        }
+        // 回答をリストに追加する
+        $replies[] = $reply;
+    }
+    return $replies;
+}
+
+/**
+ * 文字リストを作成する
+ * 
+ * @param array  $characters 文字リスト
+ * @param string $pattern    パターン
+ * @return array 文字リスト
+ */
+function createLetters(array $characters, string $pattern): array
+{
+    // 文字リスト（記号などを含まない）を設定する
+    $letters = [];
+    foreach ($characters as $character) {
+        // 文字グリフを確認する
+        $characterGlyph = $character['glyph'];
+        if (preg_match($pattern, $characterGlyph) !== 1) continue;
+        // 文字を設定する
+        $letter = [];
+        $letter['code'] = $character['unicode'];
+        $letter['type'] = $character['type'];
+        $letter['value'] = $character['glyph'];
+        // 文字をリストに追加する
+        $letters[] = $letter;
+    }
+    return $letters;
+}
--- tankore/index.js
+++ tankore/index.js
@@ -1,33 +1,45 @@
-const INPUT_DATA_LENGTH = 5;
-const GET_INIT_PARAM_PATH = '/tankore/api/index.php';
-const POPUP_TYPE_INFO = 'info';
-const POPUP_TYPE_SUCCESS = 'success';
-const POPUP_TYPE_ERROR = 'error';
-const POPUP_DISPLAY_SECOND = 2;
+const ERROR_MASSAGE = 'エラーが発生しました。時間をおいて再度操作を行ってください。';
+const API_PATH = '/tankore/api/index.php';
 
-async function sendToApi(path, method, json = {}) {
+async function sendToApi(path, init) {
     const url = new URL(window.location.origin);
     url.pathname = path;
-    const response = await fetch(url.href, {
-        method: method,
-        // headers: {
-        //     'Content-Type': 'application/json',
-        // },
-        // body: JSON.stringify(json),
-    });
-    return response.json();
+    const response = await fetch(url.href, init);
+    const data = await response.json()
+    if (response.ok) {
+        return Promise.resolve(data);
+    } else {
+        const message = data.errorMessage;
+        throw new Error(message);
+    }
 };
 
-function popupMessage(type, message) {
+async function sendGetToApi(path) {
+    const init = {
+        method: 'GET',
+    };
+    return sendToApi(path, init);
+}
+
+async function sendPostToApi(path, json = {}) {
+    const init = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(json),
+    };
+    return sendToApi(path, init);
+}
+
+function popupMessage(type, message, seconds = 2) {
     const popupArea = document.getElementById('popupArea');
     const row = document.createElement('div');
     row.classList.add('row');
     const col = document.createElement('div');
     col.classList.add('col');
     const content = document.createElement('div');
-    if (type === POPUP_TYPE_INFO) content.classList.add('info');
-    if (type === POPUP_TYPE_SUCCESS) content.classList.add('success');
-    if (type === POPUP_TYPE_ERROR) content.classList.add('error');
+    if (type !== null) content.classList.add(type);
     const span = document.createElement('span');
     const text = document.createTextNode(message);
     span.append(text);
@@ -36,7 +48,7 @@ function popupMessage(type, message) {
     row.append(col);
     const timeoutId = window.setTimeout(() => {
         row.remove();
-    }, POPUP_DISPLAY_SECOND * 1000);
+    }, seconds * 1000);
     row.addEventListener('click', () => {
         window.clearTimeout(timeoutId);
         row.remove();
@@ -45,169 +57,148 @@ function popupMessage(type, message) {
 };
 
 function popupInfoMessage(message) {
-    const type = POPUP_TYPE_INFO;
-    popupMessage(type, message);
+    popupMessage('info', message);
 };
 
 function popupSuccessMessage(message) {
-    const type = POPUP_TYPE_SUCCESS;
-    popupMessage(type, message);
+    popupMessage('success', message);
 };
 
 function popupErrorMessage(message) {
-    const type = POPUP_TYPE_ERROR;
-    popupMessage(type, message);
+    popupMessage('error', message, 10);
 };
 
-function createInputData(val) {
+function convertToLetter(val) {
     return {
-        key: val.key,
+        key: val.code,
+        type: val.type,
         value: val.value,
-        isDisabled: val.isDisabled,
-        numberOfMatches: null,
-        numberOfExists: null,
+        numberOfMatched: null,
+        numberOfContained: null,
         get state() {
-            if (this.numberOfMatches === null) return 'none';
-            if (this.numberOfExists === null) return 'none';
-            if (this.numberOfExists === 0) return 'wrong';
-            return 'right';
+            if (this.numberOfMatched === null) return 'none';
+            if (this.numberOfContained === null) return 'none';
+            if (this.numberOfMatched > 0) return 'matched';
+            if (this.numberOfContained > 0) return 'contained';
+            return 'wrong';
         },
-        get matchRate() {
-            if (this.numberOfMatches === null) return 0;
-            if (this.numberOfExists === null) return 0;
-            if (this.numberOfExists === 0) return 0;
-            const rate = this.numberOfMatches / this.numberOfExists;
-            return Math.round(rate * 100) / 100;
+        get isVisible() {
+            const types = ['seion', 'hatuon', 'tyouon'];
+            return types.includes(this.type);
         },
     };
 };
 
-function createInputCols(val) {
-    const cols = [];
-    val.forEach((letter, index) => {
-        const colIndex = Math.floor(index / INPUT_DATA_LENGTH);
-        while (cols.length <= colIndex) {
-            cols.push({ datas: [] });
-        }
-        const inputData = createInputData(letter);
-        cols[colIndex].datas.push(inputData);
-    });
-    return cols;
+function createLetters(vals) {
+    if (vals === null) return [];
+    const letters = vals.map(convertToLetter);
+    return letters;
 };
 
-function creatOutputData(val) {
+function convertToReply(val) {
     return {
-        key: val.key,
         value: val.value,
-    };
-};
-
-function creatResultData(val) {
-    return {
-        key: val.key,
-        value: val.value,
-        isMatch: null, //val.isMatch,
-        numberOfExists: null, //val.numberOfExists,
+        isMatched: val.matchesAnswerLetter,
+        numberOfContained: val.inAnswerWord,
+        get isContained() {
+            if (this.numberOfContained === null) return false;
+            return (this.numberOfContained > 0);
+        },
         get state() {
-            if (this.isMatch === null) return 'none';
-            if (this.numberOfExists === null) return 'none';
-            if (this.numberOfExists === 0) return 'wrong';
-            if (!this.isMatch) return 'right';
-            return 'match';
+            if (this.isMatched) return 'matched';
+            if (this.isContained) return 'contained';
+            return 'none';
         },
     };
+};
+
+function createReplies(vals) {
+    if (vals === null) return [];
+    const replies = vals.map(e => e.map(convertToReply));
+    return replies;
 };
 
 const tankore = {
     data: function () {
         return {
-            numberOfLetters: 0,
-            numberOfAnswers: 0,
-            letters: [{ key: '', value: '', isDisabled: false }],
-            input: { cols: [/** { datas: [Input Data Objects] } */] },
-            output: {
-                isAvailable: false,
-                datas: [/** Output Data Objects */],
-            },
-            result: { answers: [/** { datas: [Result Data Objects] } */] },
+            numberOfAnswerLetters: 0,
+            answerLimit: 0,
+            targetDate: '',
+            replies: [],
+            letters: [],
+            inputLetters: [],
         };
     },
     methods: {
+        toColumns: function (vals, size) {
+            const columns = [];
+            const max = vals.length;
+            for (let i = 0; i < max; i = i + size) {
+                const column = vals.slice(i, i + size);
+                columns.push(column);
+            }
+            return columns;
+        },
         frontValue: function (key) {
-            if (!this.output.isAvailable) {
-                popupInfoMessage('回答数が上限に達しました。');
-                return;
-            }
-            if (this.numberOfRemainingLetters <= 0) {
-                popupInfoMessage('文字数が上限に達しました。');
-                return;
-            }
-            if (key === null) {
-                popupErrorMessage('想定外の操作が行われました。');
-                return;
-            }
-            const letter = this.letters.find(e => e.key === key);
+            if (!this.isRepliable) return;
+            const letter = this.letters.filter(e => e.key !== null).find(e => e.key === key);
             if (letter === undefined) {
                 popupErrorMessage('想定外の操作が行われました。');
                 return;
             }
-            if (letter.isDisabled) {
-                popupErrorMessage('想定外の操作が行われました。');
-                return;
-            }
-            const outputData = creatOutputData(letter);
-            this.output.datas.push(outputData);
+            this.inputLetters.push(letter);
         },
         backSpace: function () {
-            if (!this.output.isAvailable) {
-                popupInfoMessage('回答数が上限に達しました。');
-                return;
-            }
-            if (this.numberOfRemainingLetters >= this.numberOfLetters) {
-                return;
-            }
-            this.output.datas.pop();
+            if (!this.isRepliable) return;
+            this.inputLetters.pop();
         },
         answer: function () {
-            if (!this.output.isAvailable) {
-                popupInfoMessage('回答数が上限に達しました。');
-                return;
-            }
-            if (this.numberOfRemainingLetters != 0) {
-                popupInfoMessage('文字数が不足しています。');
-                return;
-            }
-            this.output.isAvailable = false;
-            const outputDatas = this.output.datas;
-            const resultDatas = outputDatas.map(creatResultData);
-            this.result.answers.push({ datas: resultDatas });
-            this.output.datas = [];
-            popupInfoMessage('回答数の残り：' + this.numberOfRemainingAnswers);
-            if (this.numberOfRemainingAnswers > 0) {
-                this.output.isAvailable = true;
-            }
+            if (!this.isRepliable) return;
+            const json = {
+                answerDate: this.targetDate,
+                inputWord: this.inputWord,
+            };
+            const self = this;
+            sendPostToApi(API_PATH, json).then(data => {
+                if (data.isFailedToReply) {
+                    popupInfoMessage('単語を見つけられませんでした。');
+                }
+                self.numberOfAnswerLetters = data.numberOfAnswerLetters;
+                self.answerLimit = data.answerLimit;
+                self.targetDate = data.targetDate;
+                self.replies = createReplies(data.replies);
+                self.letters = createLetters(data.letters);
+                self.inputLetters = [];
+                popupInfoMessage('回答数の残り：' + this.numberOfRemainingReplies);
+            }).catch(error => {
+                console.error(error);
+                popupErrorMessage(ERROR_MASSAGE);
+            });
         },
     },
     computed: {
-        numberOfRemainingLetters: function () {
-            if (!this.output.isAvailable) return 0;
-            return this.numberOfLetters - this.output.datas.length;
+        inputWord: function () {
+            return this.inputLetters.map(e => e.value).join('');
         },
-        numberOfRemainingAnswers: function () {
-            const outputLength = this.output.isAvailable ? 1 : 0;
-            return this.numberOfAnswers - this.result.answers.length - outputLength;
+        isRepliable: function () {
+            return (this.answerLimit > this.replies.length);
+        },
+        numberOfRemainingReplies: function () {
+            return this.answerLimit - this.replies.length;
         },
     },
     created: function () {
         const self = this;
-        sendToApi(GET_INIT_PARAM_PATH, 'GET').then(initParam => {
-            self.numberOfLetters = initParam.numberOfLetters;
-            self.numberOfAnswers = initParam.numberOfAnswers;
-            self.letters = initParam.letters;
-            const inputCols = createInputCols(self.letters);
-            self.input.cols = inputCols;
-            self.output.isAvailable = true;
-            self.result.answers = [];
+        sendGetToApi(API_PATH).then(data => {
+            self.numberOfAnswerLetters = data.numberOfAnswerLetters;
+            self.answerLimit = data.answerLimit;
+            self.targetDate = data.targetDate;
+            self.replies = createReplies(data.replies);
+            self.letters = createLetters(data.letters);
+            self.inputLetters = [];
+        }).catch(error => {
+            console.error(error);
+            popupErrorMessage(ERROR_MASSAGE);
         });
     },
 };
